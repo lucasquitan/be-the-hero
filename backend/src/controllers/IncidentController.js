34f6b20d0768b4a11b97fff4c02e2862b5653ce8
Lucas Quintanilha <l.quitan@gmail.com>
2020-03-27 18:08:01 -0300

    updates

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -3,17 +3,15 @@ const connection = require('../database/connection');
 module.exports = {
   async create(req, res) {
     const { title, description, value } = req.body;
-
-    const ong_id = req.headers.authorization;
-
+    const ong_id = req.headers.authorization;    
+    
     const [id] = await connection('incidents').insert({
-      title,
-      description,
-      value,
-      ong_id,
+        title,
+        description,
+        value,
+        ong_id,
     });
-
-    return res.json({ id });
+    return res.json({ id });    
   },
 
   async index(req, res) {
